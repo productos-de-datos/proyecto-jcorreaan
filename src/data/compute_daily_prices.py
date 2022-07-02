21204a4abc4e8c4f90590ac7712afa8b600baf84
Jalena Correa <95106060+jcorreaan@users.noreply.github.com>
2022-07-02 18:18:57 -0500

    v1

--- src/data/compute_daily_prices.py
+++ src/data/compute_daily_prices.py
@@ -15,6 +15,17 @@ def compute_daily_prices():
     anios_mes_agrupada = df.groupby('fecha').mean({"precio": "precio"}).reset_index()
     #anios_mes_agrupada
     anios_mes_agrupada.to_csv("data_lake/business/precios-diarios.csv", index=None, header=True)
+    
+    #route_try = True
+    #try:
+    #    anios_mes_agrupada = pd.read_csv("./data_lake/cleansed/precios-horarios.csv")
+    #except FileNotFoundError:
+    # route_try = False
+    #route = "data_lake/business/precios-diarios.csv" if route_try else "../../data_lake/business/precios-diarios.csv"
+        #anios_mes_agrupada .to_csv(route, index=False)    
+    #anios_mes_agrupada= pd.read_csv("../../data_lake/cleansed/precios-horarios.csv")
+
+
 
 # raise NotImplementedError("Implementar esta función")
 
@@ -22,5 +33,5 @@ if __name__ == "__main__":
 
     import doctest
     compute_daily_prices()   
-    doctest.testmod()S
+    doctest.testmod()
      
