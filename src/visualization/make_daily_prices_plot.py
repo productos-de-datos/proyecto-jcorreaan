ded5c28a88530345fad516bef58618736aa41448
Jalena Correa <95106060+jcorreaan@users.noreply.github.com>
2022-07-02 18:22:59 -0500

    v4

--- src/visualization/make_daily_prices_plot.py
+++ src/visualization/make_daily_prices_plot.py
@@ -8,14 +8,14 @@ def make_daily_prices_plot():
     lines que representa los precios promedios diarios.
     El archivo se debe salvar en formato PNG en data_lake/business/reports/figures/daily_prices.png.
     """
-    df_daily_prices = pd.read_csv("./data_lake/business/precios-diarios.csv")
+    df = pd.read_csv("./data_lake/business/precios-diarios.csv")
     df['fecha'] = pd.to_datetime(df['fecha'])
     x = df['fecha']
     y = df['precio']
     plt.figure(figsize=(12, 8)) 
     plt.plot(x, y, label='Promedio Diario') 
     plt.savefig("./data_lake/business/reports/figures/daily_prices.png") 
-    
+
 if __name__ == "__main__":
     import doctest
     make_daily_prices_plot()
