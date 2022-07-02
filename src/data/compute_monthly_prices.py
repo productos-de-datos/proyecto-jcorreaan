790449920a82ef9d22bf23304de7123b8eca6720
Jalena Correa <95106060+jcorreaan@users.noreply.github.com>
2022-07-02 09:06:42 -0500

    mont

--- src/data/compute_monthly_prices.py
+++ src/data/compute_monthly_prices.py
@@ -18,7 +18,6 @@ def compute_monthly_prices():
     df['fecha'] =pd.to_datetime(df['fecha'], format='%Y-%m-%d')
     anios_mes_agrupada=df.groupby(df['fecha'].dt.to_period('M'))[ 'precio'].mean().reset_index()
     anios_mes_agrupada['fecha'] =pd.to_datetime(anios_mes_agrupada["fecha"].astype(str), format='%Y-%m-%d')
-    
     anios_mes_agrupada.to_csv('data_lake/business/precios-mensuales.csv', index=None)
 
     # raise NotImplementedError("Implementar esta función")
